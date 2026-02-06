2967ccc9acd0c35716b637cbfa2490d63183ad62
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-02-06 08:22:31 -0300

    OrderItem, many to many association

---
+++ src/main/java/com/pedro/course/entities/pk/OrderItemPk.java
@@ -0,0 +1,46 @@
+package com.pedro.course.entities.pk;
+
+import com.pedro.course.entities.Order;
+import com.pedro.course.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPk implements Serializable {
+    @ManyToOne
+    @JoinColumn(name = "order_Id")
+    private Order order;
+    @ManyToOne
+    @JoinColumn(name = "product_Id")
+    private Product product;
+
+    public Order getOrder(){
+        return order;
+    }
+    public void setOrder(Order order){
+        this.order = order;
+    }
+
+    public Product getProduct(){
+        return product;
+    }
+    public void setProduct(Product product){
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPk that = (OrderItemPk) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
